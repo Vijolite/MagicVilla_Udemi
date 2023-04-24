91b179195784f55ca423af06df53172508cd5943
Ija Saporenkova <ija.sha@gmail.com>
2023-04-24 15:48:56 +0100

    Add validation

--- MagicVilla_VillaAPI/Models/Dto/VillaDto.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDto.cs
@@ -1,8 +1,12 @@
-﻿namespace MagicVilla_VillaAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.Dto
 {
     public class VillaDto
     {
         public int Id { get; set; }
+        [Required] // this build-in validation connected to [ApiController]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
